18b2de93ff54e43436716001c9151514df0e0292
phamtam <phamthanhtam026@gmail.com>
2025-01-13 20:58:44 +0700

    Video_02

--- src/components/ChildComponent.jsx
+++ src/components/ChildComponent.jsx
@@ -18,10 +18,22 @@ export default ChildComponent
 // Class Component
 import React from "react";
 class ChildComponent extends React.Component {
+  state = {
+    name: "Eric",
+    channel: "HoiDanIT",
+    age: 20,
+  };
   render() {
     return (
       // JSX
-      <p>Hello ChildComponent with React</p>
+      <div>
+        <p>Hello ChildComponent with React</p>
+        <p>{JSON.stringify(this.state)}</p>
+        <p>
+          I am {this.state.name} and age: {this.state.age}. My channel is
+          {this.state.channel}
+        </p>
+      </div>
     );
   }
 }
